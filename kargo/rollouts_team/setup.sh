3001943baa00e295c44d36de662818755ea52175
Eddie Webbinaro <5262154+eddiewebb@users.noreply.github.com>
2025-10-14 10:25:30 -0400

    add nginx ingress

--- kargo/rollouts_team/setup.sh
+++ kargo/rollouts_team/setup.sh
@@ -1,11 +1,9 @@
 
-GITHUB_USER="eddiewebb"
-KARGO_QUICKSTART_PAT=$GH_AKUITY_DEMO_PAT # Replace with your actual
-GITHUB_REPOSITORY="argodemo-rollouts-app"
-PROJECT_NAME="rollouts-app"
+export GITHUB_USER="eddiewebb"
+export KARGO_QUICKSTART_PAT=$GH_AKUITY_DEMO_PAT # Replace with your actual
+export GITHUB_REPOSITORY="argodemo-rollouts-app"
+export PROJECT_NAME="rollouts-app"
 
-
-kargo apply -f project.yaml
 
 kargo create credentials github-creds \
 --project $PROJECT_NAME --git \
@@ -17,7 +15,3 @@ kargo create credentials ghcr-creds \
 --project $PROJECT_NAME --image \
 --username ${GITHUB_USER} --password ${KARGO_QUICKSTART_PAT} \
 --repo-url ghcr.io/${GITHUB_USER}/rollouts
-
-kargo apply -f warehouse.yaml 
-
-kargo apply -f stages.yaml
