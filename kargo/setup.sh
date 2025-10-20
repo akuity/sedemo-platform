abe348d700f0208387eb3b278f3d831c7b044d8c
Eddie Webbinaro <5262154+eddiewebb@users.noreply.github.com>
2025-10-20 09:32:48 -0400

    add oom project

--- kargo/setup.sh
+++ kargo/setup.sh
@@ -1,8 +1,7 @@
 
 export GITHUB_USER="eddiewebb"
 export KARGO_QUICKSTART_PAT=$GH_AKUITY_DEMO_PAT # Replace with your actual
-export GITHUB_REPOSITORY="argodemo-rollouts-app"
-export PROJECT_NAME="rollouts-app"
+
 
 
 kargo create credentials github-creds \
@@ -15,3 +14,10 @@ kargo create credentials ghcr-creds \
 --project $PROJECT_NAME --image \
 --username ${GITHUB_USER} --password ${KARGO_QUICKSTART_PAT} \
 --repo-url ghcr.io/${GITHUB_USER}/rollouts
+
+
+export PROJECT_NAME="rollouts-app"
+kargo create credentials ghcr-creds \
+--project oom-demo --image \
+--username ${GITHUB_USER} --password ${KARGO_QUICKSTART_PAT} \
+--repo-url ghcr.io/${GITHUB_USER}/oom
