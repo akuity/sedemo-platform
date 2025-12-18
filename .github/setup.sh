bd596c4f138f46adc28c3e255c12baa1df5cf015
Eddie Webbinaro <5262154+eddiewebb@users.noreply.github.com>
2025-12-18 12:38:09 -0500

    drop se teams from helm and move to custom

--- .github/setup.sh
+++ .github/setup.sh
@@ -44,11 +44,20 @@ for project in $projects; do
     echo -e "\tCreating GH Webhook"
     wh_url=`kargo get projectconfig --project $project -ojson | jq -r '.status.webhookReceivers[] | select(.name == "gh-wh-receiver").url'`
     echo -e "\tURL: $wh_url"
-    curl -L \
+    curl -Ls \
           -X POST \
           -H "Accept: application/vnd.github+json" \
           -H "Authorization: Bearer ${GH_PAT}" \
           -H "X-GitHub-Api-Version: 2022-11-28" \
-          api.github.com/repos/$GITHUB_ORG/sedemo-rollouts-app/hooks \
+          "https://api.github.com/repos/$GITHUB_ORG/sedemo-rollouts-app/hooks" \
           -d '{"name":"Kargo-Webhook","active":true,"events":["push"],"config":{"url":"'$wh_url'","content_type":"json","secret":"thisisverysecret"}}'
+    echo<<<EOF
+    -X POST \
+          -H "Accept: application/vnd.github+json" \
+          -H "Authorization: Bearer ${GH_PAT}" \
+          -H "X-GitHub-Api-Version: 2022-11-28" \
+          "https://api.github.com/repos/$GITHUB_ORG/sedemo-rollouts-app/hooks" \
+          -d '{"name":"Kargo-Webhook","active":true,"events":["push"],"config":{"url":"'$wh_url'","content_type":"json","secret":"thisisverysecret"}}'
+    EOF
+    exit
 done
